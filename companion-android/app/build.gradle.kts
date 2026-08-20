2904fa4518f5efe4425afa541e77256d027879b9
notoxus <summerthinh3@gmail.com>
2026-08-20 13:44:01 +0700

    update: mozilla core supported

--- companion-android/app/build.gradle.kts
+++ companion-android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.videodownloader.companion"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
+        versionCode = 6
         versionName = "1.0.6"
     }
 
